42dad2f1fca7ef494c464ef0d97180a05fdd3acc
bushibenxin <55702739+bushibenxin@users.noreply.github.com>
2020-01-07 22:47:20 +0800

    Add files via upload

---
+++ CODE/p86-3.c
@@ -0,0 +1,15 @@
+switch(分支变量 n)      
+{       
+	case x:
+	语句 1;     
+	break;      
+	case y:
+	语句 2;    
+	break;       
+	default:
+	语句 3;   
+	break;       
+}       
+//n = x 时   -> 只运行语句 1        
+//n = y 时   -> 只运行语句 2        
+//n ≠ x 且 n ≠ y 时 -> 运行语句 3
---
+++ CODE/p87-3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	[ Óï¾ä ;]
+	return 0;
+}
