967066b9f6a8fac305d1d18833fe321baea50ae4
bushibenxin <55702739+bushibenxin@users.noreply.github.com>
2020-01-07 22:46:24 +0800

    Add files via upload

---
+++ CODE/p83.c
@@ -0,0 +1,4 @@
+//int 类型变量的声名及使用示例
+	int a;    
+	int b = 10;
+	int c = a+b;
---
+++ CODE/p84-2.c
@@ -0,0 +1,6 @@
+//float 类型变量的声名及使用示例 
+	float a = 10;
+	float b = 3;
+	float c = a/b;
+	float d = 10/3;
+	float e = 10.0/3.0;
